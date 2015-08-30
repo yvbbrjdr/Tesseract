641fe1f04a4176a27e82951a88fc9e11b15e678e
yvbbrjdr <xyyqzy@hotmail.com>
2015-08-30 16:35:51 +0800

    Delete an extra line

--- Tesseract/Sound.h
+++ Tesseract/Sound.h
@@ -8,7 +8,6 @@
 class Sound {
 public:
     static void SetListenerValues(Coordinate position,Coordinate face,Coordinate up);
-    static int RegisterBlock(QString,Coordinate,QString,bool);
     static HSTREAM AddNewSound(Coordinate,QString);
     static void RemoveASound(HCHANNEL);
     static void PauseASound(HCHANNEL);
