6718e2590f3b1fb6f5f2bac7ba93a4c5d423eca2
yvbbrjdr <xyyqzy@hotmail.com>
2015-10-03 21:08:28 +0800

    Json is ready

--- Tesseract/Socket.cpp
+++ Tesseract/Socket.cpp
@@ -6,13 +6,21 @@ Socket::Socket(qintptr socketDescriptor,QObject *parent) : QTcpSocket(parent) {
         socketID=socketDescriptor;
     }
     connect(this,SIGNAL(readyRead()),this,SLOT(thisReadData()));
+    connect(this,SIGNAL(sendVariantMap(QVariantMap,int)),this,SLOT(setVariantMap(QVariantMap,int)));
 }
 
-void Socket::sendData(const QByteArray &data, const int id) {
-    if (id==socketID||id==-1)
-        write(data);
+void Socket::setVariantMap(const QVariantMap &data, const int id) {
+    if (id==socketID||id==-1) {
+        write(QJsonDocument::fromVariant(data).toJson());
+    }
 }
 
 void Socket::thisReadData() {
-    emit readData(socketID,peerAddress().toString(),peerPort(),readAll());
+    buffer+=readAll();
+    int pos=buffer.indexOf('}');
+    if (pos>=0) {
+        QString t=buffer.mid(0,pos+1);
+        buffer=buffer.mid(pos+1,buffer.size()-pos);
+        emit readVariantMap(socketID,peerAddress().toString(),peerPort(),QJsonDocument::fromJson(t.toUtf8()).toVariant().toMap());
+    }
 }
--- Tesseract/loginwidget.cpp
+++ Tesseract/loginwidget.cpp
@@ -1,8 +1,49 @@
 #include "loginwidget.h"
 #include "ui_loginwidget.h"
 
+QString LoginWidget::LoadFile(QString Filename) {
+    QFile file(Filename.toLocal8Bit().data());
+    if (!file.open(QIODevice::ReadOnly|QIODevice::Text))
+        return "";
+    QTextStream qts(&file);
+    QString ret=qts.readAll();
+    file.close();
+    return ret;
+}
+
+bool LoginWidget::SaveFile(QString Filename,QString Content) {
+    QFile file(Filename);
+    if (!file.open(QIODevice::ReadWrite|QIODevice::Text))
+        return 0;
+    QTextStream qts(&file);
+    qts<<Content;
+    file.close();
+    return 1;
+}
+
+bool LoginWidget::LoadConfigFile() {
+    QString Config=LoadFile("TesseractServer.conf");
+    if (Config=="")
+        return 0;
+    QVariantMap qvm=QJsonDocument::fromJson(Config.toUtf8()).toVariant().toMap();
+    ui->lineEdit->setText(qvm["server"].toString());
+    ui->lineEdit_2->setText(qvm["port"].toString());
+    ui->lineEdit_3->setText(qvm["name"].toString());
+    return 1;
+}
+
+bool LoginWidget::SaveConfigFile() {
+    QVariantMap qvm;
+    qvm.insert("server",ui->lineEdit->text());
+    qvm.insert("port",ui->lineEdit_2->text());
+    qvm.insert("name",ui->lineEdit_3->text());
+    return SaveFile("TesseractServer.conf",QJsonDocument::fromVariant(qvm).toJson().data());
+}
+
 LoginWidget::LoginWidget(QWidget *parent) : QMainWindow(parent),ui(new Ui::LoginWidget) {
     ui->setupUi(this);
+    if (!LoadConfigFile())
+        SaveConfigFile();
 }
 
 LoginWidget::~LoginWidget() {
@@ -14,16 +55,27 @@ void LoginWidget::on_pushButton_2_clicked() {
 }
 
 void LoginWidget::on_pushButton_clicked() {
+    SaveConfigFile();
     Socket *TheSocket=new Socket(-1);
     TheSocket->connectToHost(ui->lineEdit->text(),ui->lineEdit_2->text().toUInt());
-    QByteArray qba(ui->lineEdit_3->text().toLocal8Bit().data());
-    TheSocket->sendData(qba);
+    ui->label->setText("Connecting. . . ");
+    if (!TheSocket->waitForConnected()) {
+        QMessageBox::warning(0,"Failed","Unable to connect to server");
+        ui->label->setText("Welcome to Tesseract! ");
+        return;
+    }
+    ui->label->setText("Logging in. . . ");
     TesseractWidget *w=new TesseractWidget(TheSocket);
+    QVariantMap qvm;
+    qvm.insert("type","login");
+    qvm.insert("name",ui->lineEdit_3->text());
+    emit TheSocket->sendVariantMap(qvm,-1);
     w->showFullScreen();
     hide();
 }
 
 void LoginWidget::on_pushButton_3_clicked() {
+    SaveConfigFile();
     ServerWidget *w=new ServerWidget(ui->lineEdit_2->text().toUInt());
     w->show();
     hide();
